2b6a820d9abca957827e9767b761ca8f640171ef
eeedvan <diagramajunior@gmail.com>
2026-03-25 18:11:05 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ fetch("content/projects.json")
       const a = document.createElement("a");
       a.className = "card";
       a.href = `projeto.html?slug=${p.slug}`;
-
+      const cls = ["card"];
+      if (p.layout === "wide") cls.push("is-wide");
+      if (p.layout === "tall") cls.push("is-tall");
+      a.className = cls.join(" ");
       a.innerHTML = `
         <div class="card-media" style="background-image:url('${p.cover}')"></div>
         <div class="card-overlay"></div>
